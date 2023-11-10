6bbf02cd9c2a10db847e0ff2e6f04f8624ec02f2
Tóth Kevin <tothkevin0716@gmail.com>
2023-11-10 08:26:58 +0100

    MainActivity initial code

--- app/src/main/java/com/example/varosok/MainActivity.java
+++ app/src/main/java/com/example/varosok/MainActivity.java
@@ -3,12 +3,38 @@ package com.example.varosok;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+    private EditText editTextCountry;
+    private Button buttonSearch;
+    private Button buttonNewRecord;
 
+    public void init() {
+        editTextCountry = findViewById(R.id.editTextCountry);
+        buttonSearch = findViewById(R.id.buttonSearch);
+        buttonNewRecord = findViewById(R.id.buttonNewRecord);
+    }
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        init();
+
+        buttonSearch.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+
+        buttonNewRecord.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
     }
 }
