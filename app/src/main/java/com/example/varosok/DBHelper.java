98fc5c171f45351930ae3d7e05f89ced54a1418d
Tóth Kevin <tothkevin0716@gmail.com>
2023-11-10 09:08:20 +0100

    DBHelper initial code

---
+++ app/src/main/java/com/example/varosok/DBHelper.java
@@ -0,0 +1,31 @@
+package com.example.varosok;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+public class DBHelper extends SQLiteOpenHelper {
+    private static final String DB_NAME = "varos.db";
+    private static final int DB_VERSION = 1;
+    private static final String TABLE_NAME = "varosok";
+    private static final String COL_ID = "id";
+    private static final String COL_NAME = "nev";
+    private static final String COL_COUNTRY = "orszag";
+    private static final String COL_POPULATION = "lakossag";
+    public DBHelper(Context context) { super(context, DB_NAME, null, DB_VERSION); }
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        String sql = "CREATE TABLE " + TABLE_NAME + " (" +
+                COL_ID + " INTEGER PRIMARY KEY AUTOINCREMENT, " +
+                COL_NAME + " TEXT NOT NULL, " +
+                COL_COUNTRY + " TEXT NOT NULL, " +
+                COL_POPULATION + " INTEGER NOT NULL);";
+        db.execSQL(sql);
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+        db.execSQL("DROP TABLE IF EXISTS " + TABLE_NAME);
+        onCreate(db);
+    }
+}
